c70f80bf6eb912c6f505a3dfa129ec091542b6a9
CHIYEONG <CHIYEONG@CY_LAPTOP>
2019-05-23 16:24:27 +0900

    AttDto push, GridLake

--- stubbyPlanner/src/stbplanner/dto/AtrractionDTO.java
+++ stubbyPlanner/src/stbplanner/dto/AtrractionDTO.java
@@ -1,17 +1,21 @@
 package stbplanner.dto;
 
-public class AttractionDTO {
+public class AtrractionDTO {
 	
 	//fields
 	// 여행지 - 여행지좌표, 도시좌표 추가 해야한다.
 	private int attr_id;	// 여행지 id
 	private String attr_name;	// 여행지명
+	// 나라
 	private int country_id;	// 나라 id
 	private String country_name; // 나라명
+	// 도시
 	private int city_id;		// 도시 id
 	private String city_name; // 도시명
+	// 지역
 	private int region_id;	// 지역 id
 	private String region_name; // 지역명
+	// 테마 
 	private int theme_id; // 테마 id
 	private String theme_name; // 테마명
 	
@@ -34,5 +38,10 @@ public class AttractionDTO {
 	private String member_id; // 회원ID -- 임의수정
 	private String r_content; // 리뷰내용
 	private int rating; // 리뷰별점
+	
+	// 버킷리스트
+	private int bucketcity_id; // 버킷번호  
+	private String bucketcity_name; // 도시명
+	
 	
 }
