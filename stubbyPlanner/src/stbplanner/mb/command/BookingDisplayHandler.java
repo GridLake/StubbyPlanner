54e4456e76621b18af2ec30652f35fd7e56efdda
CY-LAPTOP <CY-LAPTOP@CY-LAPTOP>
2019-05-28 14:50:20 +0900

    Booking layout update, GridLake

---
+++ stubbyPlanner/src/stbplanner/mb/command/BookingDisplayHandler.java
@@ -0,0 +1,16 @@
+package stbplanner.mb.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import stbplanner.general.command.CommandHandler;
+
+public class BookingDisplayHandler implements CommandHandler{
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return "/mb/index";
+	}
+
+	
+}
